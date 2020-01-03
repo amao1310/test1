58be9bd9de297f1cb846d7687bf14f552a422f7f
Acer <Acer@Acer-pc>
2020-01-03 11:13:10 +0800

    1

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -2,10 +2,7 @@ package test;
 
 public class Test1 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		String s = "abc";
-=======
 		int a = 10;
->>>>>>> branch 'master' of https://github.com/amao1310/test1.git
 	}
 }
